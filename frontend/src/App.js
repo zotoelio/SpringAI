3528d553d15be46498f886f64cbfd1918ba125fc
eliozoto <zotoelio@msu.edu>
2025-04-20 11:31:49 -0400

    implementing the css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,32 @@
-import Recat, { useState } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
   const[activeTab, setActiveTab] = useState('image-generator');
 
   const handleTabChange = (tab) => {
-    alert(tab);
+    //alert(tab);
     setActiveTab(tab);
   }
 
   return (
     <div className="App">
-      <button onClick={() => handleTabChange('image-generator')}>Image Generator</button>
-      <button onClick={() => handleTabChange('chat')}>Chat</button>
-      <button onClick={() => handleTabChange('recipe-generator')}>Recipe Generator</button>
+      <button className={activeTab === 'image-generator' ? 'active' : ''} 
+        onClick={() => handleTabChange('image-generator')}>Image Generator
+      </button>
+      <button className={activeTab === 'chat' ? 'active' : ''} 
+        onClick={() => handleTabChange('chat')}>Chat
+      </button>
+      <button className={activeTab === 'recipe-generator' ? 'active' : ''}
+        onClick={() => handleTabChange('recipe-generator')}>Recipe Generator
+      </button>
+
+      <div>
+        {activeTab === 'image-generator' && <h2>Image Generator</h2>}
+        {activeTab === 'chat' && <h2>Chat</h2>}
+        {activeTab === 'recipe-generator' && <h2>Recipe Generator</h2>}
+      </div> 
+
     </div>
   );
 }
